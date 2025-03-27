f8c96aa13b526e764d76c754a6813aa880e20686
Vellu Sorvari <70940730+Kuoste@users.noreply.github.com>
2025-03-27 18:47:28 +0000

    adding tracking for client and server side

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let appinsights = req.app.get('appinsights');
+  appinsights.trackEvent({name: "serverCount", properties: {customProperty: "custom property value"}});
   res.render('index', { title: 'Express' });
 });
 
